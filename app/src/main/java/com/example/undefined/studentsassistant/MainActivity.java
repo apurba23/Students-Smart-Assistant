beaafe027642b13bfc6d89bad97c7591a594d00a
Apurba Sarkar <apurba.sarkar.cse@ulab.edu.bd>
2019-01-13 20:55:34 +0000

    Main Activity & Student Model Class

--- app/src/main/java/com/example/undefined/studentsassistant/MainActivity.java
+++ app/src/main/java/com/example/undefined/studentsassistant/MainActivity.java
@@ -1,17 +1,64 @@
 package com.example.undefined.studentsassistant;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.undefined.studentsassistant.R;
 
 public class MainActivity extends AppCompatActivity {
-
+    FirebaseFirestore db;
+    List studentList;
+    ListView listView;
+    CustomAdapter adapter;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        db=FirebaseFirestore.getInstance();
+        listView=findViewById(R.id.listview);
+        textView=findViewById(R.id.textView);
+        String id=getIntent().getStringExtra("id");
+        textView.setText("ID: "+id);
+        getStudentInfo(id);
+    }
+    public void getStudentInfo(String id)
+    {
+
+        db.collection("tbl_student").whereEqualTo("id",id).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot documentSnapshots) {
+                studentList= new ArrayList();
+
+                for(DocumentSnapshot doc:documentSnapshots)
+                {
+                    StudentModel model=doc.toObject(StudentModel.class);
+                    Log.d("fbase",model.toString());
+                    studentList.add(model);
+                }
+                adapter=new CustomAdapter(getBaseContext(),studentList);
+                listView.setAdapter(adapter);
+                adapter.notifyDataSetChanged();
+            }
+        });
+
     }
 
     public void LogOut(View view) {
+        Intent intent=new Intent(MainActivity.this,LoginActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/undefined/studentsassistant/StudentModel.java
@@ -0,0 +1,80 @@
+package com.example.undefined.studentsassistant;
+
+public class StudentModel {
+
+    String name;
+    String id;
+    String semester;
+    String cgpa;
+    String performance_cgpa;
+    String performance_result;
+
+    public StudentModel(String name, String id, String semester, String cgpa, String performance_cgpa, String performance_result) {
+        this.name = name;
+        this.id = id;
+        this.semester = semester;
+        this.cgpa = cgpa;
+        this.performance_cgpa = performance_cgpa;
+        this.performance_result = performance_result;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSemester() {
+        return semester;
+    }
+
+    public void setSemester(String semester) {
+        this.semester = semester;
+    }
+
+    public String getCgpa() {
+        return cgpa;
+    }
+
+    public void setCgpa(String cgpa) {
+        this.cgpa = cgpa;
+    }
+
+    public String getPerformance_cgpa() {
+        return performance_cgpa;
+    }
+
+    public void setPerformance_cgpa(String performance_cgpa) {
+        this.performance_cgpa = performance_cgpa;
+    }
+
+    public String getPerformance_result() {
+        return performance_result;
+    }
+
+    public void setPerformance_result(String performance_result) {
+        this.performance_result = performance_result;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentModel{" +
+                "name='" + name + '\'' +
+                ", id='" + id + '\'' +
+                ", semester='" + semester + '\'' +
+                ", cgpa='" + cgpa + '\'' +
+                ", performance_cgpa='" + performance_cgpa + '\'' +
+                ", performance_result='" + performance_result + '\'' +
+                '}';
+    }
+}
