eb8f6a75adb5fe49813f953511845a7e477c6d45
SanjidaSiddiqua <sanjida.siddiqua777@gmail.com>
2019-01-13 19:39:27 +0000

    Apps Layout - Child View

---
+++ app/src/main/java/com/example/undefined/studentsassistant/childview.java
@@ -0,0 +1,13 @@
+package com.example.undefined.studentsassistant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class childview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.childview);
+    }
+}
